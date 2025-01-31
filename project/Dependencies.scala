1408e2d2a47f99c88be08fe4b9a996718bc7462c
KrutikaP <21079593+KrutikaP@users.noreply.github.com>
2025-01-31 13:52:10 +0000

    Added dependancy and updated readme file

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,10 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"         % Test
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"         % Test,
+    "org.asynchttpclient"   %  "async-http-client"         % "2.12.3"        % Test,
+    "uk.gov.hmrc"           %% "uri-template"              % "1.14.0"        % Test,
+    "org.playframework"     %% "play-json"                 % "3.0.4"         % Test
   )
 
 }
